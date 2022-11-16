83b25befcc5fa825122363a5f719dce3afe71108
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2022-11-17 01:59:25 +0530

    team-players-added

--- src/components/CreateTeam.js
+++ src/components/CreateTeam.js
@@ -9,18 +9,44 @@ import Vs from "../assets/VS.svg"
 
 
 const CreateTeam = () => {
-  const [team, setTeam] = useState([])
+  const [team, setTeam] = useState([]);
+  const [teamPlayers, setTeamPlayers] = useState([]);
 
   const fetchData = async () => {
     const res = await axios.get("https://api.klutchh.in/v1/rosters/128622/129859?game_type=false")
     const data = res.data.data;
     setTeam(data)
-    console.log(data)
   }
 
   useEffect(() => {
     fetchData()
   }, [])
+
+
+  const handleCard = (item) => {
+    const creditLimit = teamPlayers.reduce((tot, item) => tot + item.credit, 0)
+
+    if (!teamPlayers.includes(item)) {
+      if (creditLimit > 15) {
+        alert("credits exceed")
+        return
+      }
+      if (creditLimit === 15) {
+        alert("credits full")
+        return
+      }
+      if (teamPlayers.length === 5) {
+        alert("add 5 players according to the given credits to your team")
+        return
+      }
+      setTeamPlayers((prev) => ([...prev, item]))
+    }
+
+    if (teamPlayers.includes(item)) {
+
+      setTeamPlayers((prev) => ([...prev.filter(el => el !== item)]))
+    }
+  }
 
   return (
     <div className="createTeamSection">
@@ -30,27 +56,25 @@ const CreateTeam = () => {
       <div className="createTeamContent">
         <div className="teamSelect">
           <div className="teamSelectPlayers">
+            {teamPlayers.length ? teamPlayers.map((item) => (
+              <div className="teamPlayerDetails">
+                {/* <img src={teamPlayer} /> */}
+              </div>
+            ))
+              : null
+            }
+          {Array(5-teamPlayers.length).fill("0").map((item) => (
             <div className="teamPlayer">
               <img src={teamPlayer} />
             </div>
-            <div className="teamPlayer">
-              <img src={teamPlayer} />
-            </div>
-            <div className="teamPlayer">
-              <img src={teamPlayer} />
-            </div>
-            <div className="teamPlayer">
-              <img src={teamPlayer} />
-            </div>
-            <div className="teamPlayer">
-              <img src={teamPlayer} />
-            </div>
+            ))}
+
           </div>
           <div className="teamPlayerText">
             <span className="teamText">Choose five players to join your team</span>
           </div>
           <div className="teamCredit">
-            <span className="teamCreditText">Credits Left : 0/15</span>
+            <span className="teamCreditText">Credits Left : {teamPlayers.reduce((tot, item) => tot + item.credit, 0)}/15</span>
           </div>
         </div>
 
@@ -85,7 +109,7 @@ const CreateTeam = () => {
         <div className="teamMembers">
           <div className="team1Box">
             {team.roster1 ? team.roster1.map((item) => (
-              <div className="playerCard">
+              <div className="playerCard" onClick={() => handleCard(item)} style={teamPlayers.includes(item) ? { border: "2px solid #FF326A" } : null}>
                 <div className="playerCardImg">
                   <img src={item.image_url ? item.image_url : "https://cdn.pandascore.co/images/player/image/17662/lowel.png"} alt="playerImg" className="playerImg" />
                 </div>
@@ -109,7 +133,7 @@ const CreateTeam = () => {
           </div>
           <div className="team2Box">
             {team.roster2 ? team.roster2.map((item) => (
-              <div className="playerCard">
+              <div className="playerCard" onClick={() => handleCard(item)} style={teamPlayers.includes(item) ? { border: "2px solid #FF326A" } : null}>
                 <div className="playerCardImg">
                   <img src={item.image_url ? item.image_url : "https://cdn.pandascore.co/images/player/image/17662/lowel.png"} alt="playerImg" className="playerImg" />
                 </div>
@@ -132,6 +156,8 @@ const CreateTeam = () => {
               : null}
           </div>
         </div>
+
+        <button className="btn">PRIVEW SELECTION</button>
 
       </div>
     </div>
